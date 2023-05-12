7776bfd0b24e910682afe917199721bb3498fe0f
CodingTeddyBearr <131594057+CodingTeddyBearr@users.noreply.github.com>
2023-05-12 10:12:43 +0800

    Add files via upload

---
+++ Tutorial/Tutorial6/T6.java
@@ -0,0 +1,37 @@
+package T6Q1;
+
+public class T6 {
+    /*Q1)C peek
+    Q2)
+    s2.push(s1.pop());  s1=[zero,one]   s2=[two]            s3=[]
+    s3.push(s1.pop());  s1=[zero]       s2=[two]            s3=[one]
+    s1.pop();           s1=[]           s2=[two]            s3=[one]
+    s1.push(s2.pop());  s1=[two]        s2=[]               s3=[one]
+    s2.push(s3.pop());  s1=[two]        s2=[one]            s3=[]
+    s2.push(s1.pop());  s1=[]           s2=[one,two]       s3=[]
+
+                 TWO
+                 ONE
+
+        S1        S2      S3
+
+     Q3) a)yes
+     push (1), pop(), push(2), pop(), push(3), pop()
+         b)yes
+     push(1), push(2), pop(), push(3), pop(), pop()
+         c)yes
+     push(1), push(2), push(3), pop(),pop(),pop()
+         d)yes
+     push(1),pop(), push(2), push(3), pop(),pop()
+
+     Q4) a) abc*+
+         b) ab*cd/-
+         c) abc*d+e/+
+
+     Q5) a) (a+b)*c
+         b) a*(b+c)
+
+     Q6) D.all of the above
+
+     */
+}
